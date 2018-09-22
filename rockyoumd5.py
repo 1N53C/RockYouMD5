1c3450d840c803e3a2a0b34076373869fb08c389
Rene Glasow <34792428+PengSec@users.noreply.github.com>
2018-09-22 10:34:11 +0200

    Create rockyoumd5.py
    The RockYou To MD5 Python Script

---
+++ rockyoumd5.py
@@ -0,0 +1,12 @@
+#! /bin/env python
+
+import hashlib as hash
+
+user_input = input("Please enter the MD5 Hash to decrypt: ")
+
+with open('rockyou.txt', 'r') as file:
+    for line in file:
+        word = hash.md5(line.strip().encode()).hexdigest()
+        if word == user_input:
+            print("[+] Password found: " + line)
+            break
