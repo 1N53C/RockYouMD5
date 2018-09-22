549019e16a51fe74e51f7464e53ca32e146055b4
Rene Glasow <34792428+PengSec@users.noreply.github.com>
2018-09-22 10:56:44 +0200

    Update rockyoumd5.py

--- rockyoumd5.py
+++ rockyoumd5.py
@@ -2,6 +2,7 @@ import hashlib as hash
 
 user_input = input("Please enter the MD5 Hash to decrypt: ")
 
+# in this case, the rockyou.txt is in the same folder as the script
 with open('rockyou.txt', 'r') as file:
     for line in file:
         word = hash.md5(line.strip().encode()).hexdigest()
