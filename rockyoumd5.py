605c5b4d539afe49351199d78e4c3e27c421acc4
Rene Glasow <34792428+PengSec@users.noreply.github.com>
2018-09-22 14:09:55 +0200

    Added sha256

--- rockyoumd5.py
+++ rockyoumd5.py
@@ -1,11 +1,17 @@
-import hashlib as hash
 
-user_input = input("Please enter the MD5 Hash to decrypt: ")
+user_input = input("Please enter the Hash to decrypt: ")
+chosen_hash = input("Enter Type of Hash (md5 or sha256): ")
+extra_chars = "$§%/()?=!"
+
 
 # in this case, the rockyou.txt is in the same folder as the script
 with open('rockyou.txt', 'r') as file:
     for line in file:
-        word = hash.md5(line.strip().encode()).hexdigest()
+        if chosen_hash == 'md5':
+            word = hash.md5(line.strip().encode()).hexdigest()
+        else:
+            word = hash.sha256(line.strip().encode()).hexdigest()
+           
         if word == user_input:
             print("[+] Password found: " + line)
             break
